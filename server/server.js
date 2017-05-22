d98003a843448e92ea458b47a3a103ab08664023
Mason Prey <masonprey7@gmail.com>
2017-05-21 21:27:51 -0500

    correctly serve static assests

--- server/server.js
+++ server/server.js
@@ -45,9 +45,10 @@ configureAuth(app)
 
 app.use('/api', authRoute)
 
+
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));

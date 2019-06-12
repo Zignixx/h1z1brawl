9fef04231e121d8f9244b515608177ec9f89654a
Mason Prey <mprey@users.noreply.github.com>
2019-06-11 23:54:18 -0500

    Update bots.js

--- bots.js
+++ bots.js
@@ -1,47 +1,9 @@
 let accounts = {}
 
 if (process.env.NODE_ENV === "production") {
-  accounts = {
-    'csgocactus_bot01': {
-      accountName: 'csgocactus_bot01',
-      password: 'CACTUSBOT_01',
-      sharedSecret: 'X8hN09X7BKjc1ot6vl6Q2Pa5Emw=',
-      identitySecret: 'YLGs9zSZoWtWsIHKN5neuCe8av4='
-    },
-    'csgocactus_bot02': {
-      accountName: 'csgocactus_bot02',
-      password: 'CACTUSBOT__02',
-      sharedSecret: 'mTQWy5hTu9WRz4smJZfCi018nXU=',
-      identitySecret: 'Uk3JSxQGDvUgp5epkKBk/tYaY1M'
-    },
-    'csgocactus_bot03': {
-      accountName: 'csgocactus_bot03',
-      password: 'CACTUSBOT___03',
-      sharedSecret: 'yWZO+I4q9BqCgNnULV5F+G0Hurw=',
-      identitySecret: 'HM9SkOSVBnSrbWwmH897tyO+SdE='
-    },
-    'csgocactus_bot04': {
-      accountName: 'csgocactus_bot04',
-      password: 'CACTUSBOT____04',
-      sharedSecret: 'pP9w6ovmE8v4fEUL/VfYIaANrhM=',
-      identitySecret: 'b2YlAtz+/NtpSXeUMb55lBJq6Xg='
-    },
-    'csgocactus_bot05': {
-      accountName: 'csgocactus_bot05',
-      password: 'CACTUSBOT_05',
-      sharedSecret: '9UUOph97QSb4C4ZRBl2QB8Je9Kk=',
-      identitySecret: 'COTyaWv4hPVE2+PQGcfvrt71QgI='
-    }
-  }
+  accounts = {}
 } else {
-  accounts = {
-    'h1z1brawl_testbot001': {
-      accountName: 'h1z1brawl_testbot001',
-      password: 'TESTBOT_01',
-      sharedSecret: 'GQPd41FUEgFz7ZMKTybFKqCDo8M=',
-      identitySecret: 'AAOs92MrHDsV7JPoQx9KY+cDphU='
-    }
-  }
+  accounts = {}
 }
 
 const settings = {
